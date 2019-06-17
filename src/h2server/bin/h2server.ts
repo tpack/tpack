4e03e5b6ddefc7d9efb89e0ef0f3092e7157f462
sayll <saytxk@gmail.com>
2019-06-17 21:31:44 +0800

    新增h2server中static参数功能

--- src/h2server/bin/h2server.ts
+++ src/h2server/bin/h2server.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+import { join } from 'path'
 import { WebServer } from '../../server/webServer'
 import { ANSIColor, color } from '../../utils/ansi'
+import { Matcher } from '../../utils/matcher'
 import { HTTPRequest, HTTPResponse } from '../../utils/httpServer'
 import { parseCommandLineArguments, formatCommandLineOptions, CommandLineOption } from '../../utils/commandLine'
 
 const commandOptions: { [option: string]: CommandLineOption } = {
-	// host
+	// Host
 	'--host': {
 		group: 'host',
 		argument: 'host',
@@ -22,7 +24,7 @@ const commandOptions: { [option: string]: CommandLineOption } = {
 		description: 'Address to use [0.0.0.0]'
 	},
 
-	// browser
+	// Browser
 	'--open': {
 		group: 'browser',
 		alias: ['-o'],
@@ -44,7 +46,7 @@ const commandOptions: { [option: string]: CommandLineOption } = {
 		description: 'Do not show directory list'
 	},
 
-	// logger
+	// Logger
 	'--utc': {
 		group: 'logger',
 		alias: ['-U'],
@@ -58,39 +60,52 @@ const commandOptions: { [option: string]: CommandLineOption } = {
 		description: 'Suppress log messages from output'
 	},
 
-	// server
+	// Server
 	'--cwd': {
 		group: 'server',
 		argument: 'path',
 		description: 'cwd'
+	},
+	'--proxy': {
+		alias: ['-P'],
+		argument: 'url',
+		description: 'Fallback proxy if the request cannot be resolved. e.g.: http://github.com'
+	},
+	'--redirect': {
+		alias: ['-R'],
+		argument: 'url',
+		description: '重写请求的地址'
+	},
+	'--static': {
+		alias: ['-S'],
+		// multiple: true,
+		argument: 'path',
+		description: '静态文件及文件夹'
 	},
 	'--root-path': {
 		argument: 'path',
 		description: ''
 	},
 	'--index': {
+		alias: ['-I'],
 		multiple: true,
 		argument: 'fileName',
 		description: 'main file'
 	},
 	'--ssl': {
-		alias: ['-S'],
 		description: 'Enable https'
 	},
 	'--cert': {
-		alias: ['-C'],
 		argument: 'filePath',
 		default: '',
 		description: 'Path to ssl cert file (default: cert.pem).'
 	},
 	'--key': {
-		alias: ['-K'],
 		argument: 'filePath',
 		default: '',
 		description: 'Path to ssl key file (default: key.pem)'
 	},
 	'--http2': {
-		alias: ['-H2'],
 		description: 'Enable http2'
 	},
 	'--max-length': {
@@ -107,7 +122,7 @@ if (argv['--help']) {
 	console.log([
 		'Usage: h2server [path] [options]',
 		'',
-		'options:',
+		'Options:',
 		formatCommandLineOptions(commandOptions)
 	].join('\n'))
 	process.exit()
@@ -123,17 +138,19 @@ const url = typeof open === 'string' ? open : '' || argv['--host'] as string || 
 
 const logger = {
 	log: argv['--silent'] ? function () {} : console.log,
-	info: (text: string, ...args: any[]) => logger.log(color(text, ANSIColor.blue), ...args),
 	request: (request: HTTPRequest, response: HTTPResponse, server: WebServer) => {
 		const date = argv['--utc'] ? new Date().toUTCString() : new Date()
 		const ip = argv['--log-ip'] ? request.headers['x-forwarded-for'] || '' + request.connection.remoteAddress : ''
-		logger.info(
+		logger.log(
 			'[%s] %s "%s %s" "%s"',
 			date, ip, color(request.method, ANSIColor.cyan), color(request.url, ANSIColor.cyan),
 			request.headers['user-agent']
 		)
 	}
 }
+const staticPath = argv['--static'] as string | undefined
+const redirect = argv['--redirect'] as string | undefined
+const proxy = argv['--proxy'] as string | undefined
 
 const server = new WebServer({
 	url,
@@ -147,10 +164,60 @@ const server = new WebServer({
 	routers: [
 		{
 			match: '**',
+			proxy: proxy,
+			static: staticPath,
+			rewrite: redirect,
+			break: !!redirect || !!proxy || false,
 			async process(req, res, server) {
+				const { path } = req
+				const { fs } = server
+				const filePath = server.mapPath(path)
+				const fileStaticPath = server.mapPath(join(staticPath || '', path))
+				const matcher = new Matcher('**' || (() => true), undefined, true)
+				if (staticPath && filePath) {
+					if (typeof staticPath === 'string') {
+						try {
+							const stat = await fs.getStat(filePath)
+							// 匹配 defaultPages
+							if (stat.isDirectory() && fileStaticPath) {
+								const entries = await fs.readDir(filePath)
+								const entriesStatic = await fs.existsDir(fileStaticPath) ? await fs.readDir(fileStaticPath) : []
+								for (const index of server.defaultPages) {
+									// 尝试匹配static内文件
+									if (!entries.includes(index) && entriesStatic.includes(index)) {
+										const indexPath = join(fileStaticPath, index)
+										if (await fs.existsFile(indexPath)) {
+											req.path = join(staticPath, path, index)
+										}
+									}
+								}
+							}
+						}
+						catch (e) {
+							// 尝试匹配static内文件
+							if (fileStaticPath) {
+								const stat = await fs.getStat(fileStaticPath)
+								if (stat.isFile()) {
+									req.path = join(staticPath, path)
+								}
+							}
+						}
+					}
+				}
+				else if (redirect) {
+					if (typeof redirect === 'string') {
+						// req.path = server.formatURL(redirect, req.path, matcher.base)
+					}
+				}
+				else if (proxy) {
+					if (typeof proxy === 'string') {
+						// req.path = server.formatURL(proxy, req.path, matcher.base) + (req.search ? '?' + req.search : '')
+						// server.writeProxy(req, res, )
+					}
+				}
+
 				logger.request(req, res, server)
-			},
-			break: false
+			}
 		},
 		{
 			match: '*.ejs',
